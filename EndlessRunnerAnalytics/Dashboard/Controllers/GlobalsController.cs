9c26b1ebd1f483ec55bcc05b272f4e7ff24cae46
SuperJura <jura1994jk@hotmail.com>
2018-06-19 11:35:31 +0200

    Added globals page

---
+++ EndlessRunnerAnalytics/Dashboard/Controllers/GlobalsController.cs
@@ -0,0 +1,37 @@
+﻿using Dashboard.Services;
+using EndlessRunner.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class GlobalsController : Controller
+	{
+		IEndlessRunnerDAL dal = new EndlessRunnerDAL();
+		// GET: Globals
+		public ActionResult Index()
+		{
+			float timePlayedInSeconds = 0;
+			List<EndGameSession> sessions = dal.GetAllEndGameSessions();
+			for (int i = 0; i < sessions.Count; i++)
+			{
+				timePlayedInSeconds += sessions[i].GameTime;
+			}
+			TimeSpan timePlayed = TimeSpan.FromSeconds(timePlayedInSeconds);
+
+			int numOfPickups = 0;
+			List<Pickup> pickups = dal.GetAllPickups();
+			for (int i = 0; i < pickups.Count; i++)
+			{
+				numOfPickups += pickups[i].PickupCount;
+			}
+
+			ViewBag.NumOfRuns = dal.GetAllRuns().Count;
+			ViewBag.NumOfPickups = numOfPickups;
+			ViewBag.TimePlayed = timePlayed.ToString(@"hh\:mm\:ss");
+
+			return View();
+        }
+    }
+}
